cf978738d20386dba1994d3564f86cc85c4c968a
whkl <phiiipwong@163.com>
2019-09-03 13:21:56 +0800

    add run

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
-python train.py \
-	--model_def config/yolov3-custom.cfg \
-	--data_config config/custom.data \
-	--pretrained_weights weights/darknet53.conv.74 \
-	--n_cpu 2 \
-	--checkpoint_interval 5 \
-	--evaluation_interval 5 \
-	--batch_size 8
+python -m torch.distributed.launch \
+	--nproc_per_node=4 \
+	train.py \
+		--model_def config/yolov3-custom.cfg \
+		--data_config config/custom.data \
+		--pretrained_weights weights/darknet53.conv.74 \
+		--n_cpu 4 \
+		--checkpoint_interval 5 \
+		--evaluation_interval 5 \
+		--batch_size 8 \
+		--img_size 608 \
+		--gradient_accumulations 1
